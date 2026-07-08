723fabf09ffe8ccdf232ab2d040463c8033f482c
benin9 <pramditabening.com>
2026-07-08 22:32:01 +0700

    Force strict TCP hostname for internal database network

--- backend-django/core/settings.py
+++ backend-django/core/settings.py
@@ -89,17 +89,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 IS_RAILWAY = os.environ.get('RAILWAY_ENVIRONMENT_NAME') is not None or os.environ.get('PORT') is not None
 
 if IS_RAILWAY:
-    # Menggunakan kredensial Postgres Railway secara eksplisit lintas kontainer
+    # Kita hardcode HOST internal network Railway agar tidak mencari local socket Unix
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('PGDATABASE') or os.environ.get('POSTGRES_DB') or 'railway',
-            'USER': os.environ.get('PGUSER') or os.environ.get('POSTGRES_USER') or 'postgres',
-            'PASSWORD': os.environ.get('PGPASSWORD') or os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('PGHOST') or os.environ.get('POSTGRES_HOST'),
-            'PORT': os.environ.get('PGPORT') or os.environ.get('POSTGRES_PORT') or '5432',
+            'NAME': os.environ.get('PGDATABASE') or 'railway',
+            'USER': os.environ.get('PGUSER') or 'postgres',
+            'PASSWORD': os.environ.get('PGPASSWORD'),
+            # Jika variabel PGHOST kosong, kita paksa tembak ke alamat internal service database
+            'HOST': os.environ.get('PGHOST') or 'postgres.railway.internal' or 'db.railway.internal',
+            'PORT': os.environ.get('PGPORT') or '5432',
         }
     }
+    
 else:
     # Setelan MySQL lokal untuk laptop kamu saat development
     DATABASES = {
