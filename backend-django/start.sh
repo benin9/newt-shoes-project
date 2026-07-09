b0dc79de909852e7a7bdbd8760b3c378f81e57dc
benin9 <pramditabening.com>
2026-07-09 11:28:07 +0700

    fix: simplify Dockerfile

--- backend-django/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-python manage.py migrate --noinput
-python manage.py shell -c "exec(open('init_users.py').read())"
-exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --preload --timeout 300 --access-logfile - --error-logfile -
